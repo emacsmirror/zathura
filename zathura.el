754664519f0c346a296d1fa81f3243c584eaef66
treflip <treflcrew@gmail.com>
2025-07-21 14:57:58 +0400

    fix formatting for melpa

--- zathura.el
+++ zathura.el
@@ -24,7 +24,10 @@
 
 
 ;;; Commentary:
-;; A set of simple commands that create hyperlinks to documents open in Zathura document viewer.
+
+;; Zathura <https://pwmt.org/projects/zathura/> is a highly customizable and
+;; functional document viewer, external to Emacs.  This package provides a set
+;; of simple commands for creating hyperlinks to open documents in Zathura.
 
 ;;; Code:
 
@@ -37,65 +40,65 @@
 
 (defun zathura--get-procs ()
   (cl-remove-if-not (lambda (p) (string-match "zathura" p))
-					(dbus-list-names :session)))
+                    (dbus-list-names :session)))
 
 
 (defun zathura--dump-interface ()
   (dbus-introspect-get-interface :session
-								 (elt (zathura--get-procs) 0)
-								 zathura-service-path
-								 zathura-service-iname))
+                                 (elt (zathura--get-procs) 0)
+                                 zathura-service-path
+                                 zathura-service-iname))
 
 
 (defun zathura--annotate-candidate (proc)
   ;; replace with (format ...) ?
   (propertize (concat "   page "
-					  (format "%s" (dbus-get-property :session
-														  proc
-														  zathura-service-path
-														  zathura-service-iname
-														  "pagenumber"))
-					  " "
-					  (dbus-get-property :session
-										 proc
-										 zathura-service-path
-										 zathura-service-iname
-										 "filename"))
-			  'face 'font-lock-comment-face))
+                      (format "%s" (dbus-get-property :session
+													  proc
+													  zathura-service-path
+													  zathura-service-iname
+													  "pagenumber"))
+                      " "
+                      (dbus-get-property :session
+                                         proc
+                                         zathura-service-path
+                                         zathura-service-iname
+                                         "filename"))
+              'face 'font-lock-comment-face))
 
 
 (defun zathura--pick-process (procs)
   (completing-read "Select process: "
-				   (lambda (str pred action)
-					 (if (eq action 'metadata)
-						 `(metadata
-						   (annotation-function . zathura--annotate-candidate))
-					   (complete-with-action action procs str pred)))))
+                   (lambda (str pred action)
+                     (if (eq action 'metadata)
+                         `(metadata
+                           (annotation-function . zathura--annotate-candidate))
+                       (complete-with-action action procs str pred)))))
 
 
 (defun zathura-get-link-details ()
   (let* ((procs (zathura--get-procs))
-		 (service)
-		 (page)
-		 (file))
-	(cond ((= 1 (length procs))
-		   (setq service (car procs)))
-		  ((> (length procs) 1)
-		   (setq service (zathura--pick-process procs)))
-		  ((< (length procs) 1)
-		   (error "Zathura is not running.")))
-	(setq page (dbus-get-property :session service zathura-service-path
-								  zathura-service-iname "pagenumber"))
-	(setq file (dbus-get-property :session service zathura-service-path
-								  zathura-service-iname "filename"))
-	(cons file page)))
+         (service)
+         (page)
+         (file))
+    (cond ((= 1 (length procs))
+           (setq service (car procs)))
+          ((> (length procs) 1)
+           (setq service (zathura--pick-process procs)))
+          ((< (length procs) 1)
+           (error "Zathura is not running.")))
+    (setq page (dbus-get-property :session service zathura-service-path
+                                  zathura-service-iname "pagenumber"))
+    (setq file (dbus-get-property :session service zathura-service-path
+                                  zathura-service-iname "filename"))
+    (cons file page)))
 
 
 (defun zathura (file &optional page)
   "Call zathura with the given `file' and `page'."
   (if page
-	  (call-process "zathura" nil 0 nil "-P" (format "%s" page) file)
-	(call-process "zathura" nil 0 nil file)))
+      (call-process "zathura" nil 0 nil "-P" (format "%s" page) file)
+    (call-process "zathura" nil 0 nil file)))
 
 
 (defun zathura-insert-hy-link ()
@@ -103,14 +106,15 @@
 the chosen process of `zathura'"
   (interactive)
   (cl-destructuring-bind (file . page) (zathura-get-link-details)
-	(insert (format "<zathura \"%s\" %s>" file page))))
+    (insert (format "<zathura \"%s\" %s>" file page))))
 
 
 (defun zathura-insert-org-elisp-link ()
-  "Insert an elisp org-link to the current page from the chosen process of `zathura'"
+  "Insert an elisp org-link to the current page from the chosen process
+ of `zathura'"
   (interactive)
   (cl-destructuring-bind (file . page) (zathura-get-link-details)
-	(insert (format "[[elisp:(zathura \"%s\" %s)][%s]]"
+    (insert (format "[[elisp:(zathura \"%s\" %s)][%s]]"
 					file
 					page
 					(read-string "Description: ")))))
